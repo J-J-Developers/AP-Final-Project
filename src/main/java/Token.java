949a459bf6a422beffae47d472cd7effba5e93a4
vania <vania.v@gmail.com>
2024-06-10 20:47:26 +0330

    Token

---
+++ src/main/java/Token.java
@@ -0,0 +1,14 @@
+import java.util.UUID;
+
+public class Token {
+    private String tokenId;
+
+    public Token() {
+        this.tokenId = UUID.randomUUID().toString();
+    }
+
+    public String getTokenId() {
+        return tokenId;
+    }
+}
+
