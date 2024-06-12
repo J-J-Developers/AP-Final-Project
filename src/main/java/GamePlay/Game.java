e8d20ac798200e6b19bb50d5eb718d796aa45de7
arshiammz <mohammadzade.arshia84@gmail.com>
2024-06-12 23:48:58 +0330

    Advance divideCards method complelted whit GUI hokm selection

--- src/main/java/GamePlay/Game.java
+++ src/main/java/GamePlay/Game.java
@@ -1,5 +1,6 @@
 package GamePlay;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -10,10 +11,12 @@ public class Game{
     private Player king;
     private Card hokm;
     Random rand = new Random();
+    private final Object lock = new Object();
+    private boolean isRulerCardSelected = false;
+
     public ArrayList<Team> roomTeams = new ArrayList<>(2);
     public ArrayList<Card> roomCards = new ArrayList<>(getCardBox().cards);
     public ArrayList<Player> roomPlayers = new ArrayList<>();
-    //این اری لیستو فقط برای پخش کارت زدم نه ارتباط با سرور!!
     // Constructor
     public Game(Player p1, Player p2, Player p3, Player p4) {
         roomPlayers.add(new Player(p1.getName(),p1.getId()));
@@ -47,25 +50,74 @@ public class Game{
         this.king = king;
     }
 
+    int kingIndex = 0;
+    public void preGame(){
+        // پیدا کردن حاکم
+        for (int i = 0; i < roomPlayers.size(); i++) {
+            if (this.king == roomPlayers.get(i)){
+                kingIndex = i;
+            }
+        }
+        // دادن 5 کارت به حاکم و نفر بعدیش
+            for (int j = 0; j < 5; j++) {
+                roomPlayers.get(kingIndex).getMyCards().add(roomCards.get(rand.nextInt(roomCards.size())));
+                roomPlayers.get(kingIndex).getMyButtons().add(new JButton());
+                roomPlayers.get(kingIndex).getMyButtons().getLast().setIcon(new ImageIcon(roomPlayers.get(kingIndex).getMyCards().getLast().getRoo().getImage()));
+            }
+            for (int j = 0; j < 5; j++) {
+                roomPlayers.get((kingIndex+1)%4).getMyCards().add(roomCards.get(rand.nextInt(roomCards.size())));
+                roomPlayers.get((kingIndex+1)%4).getMyButtons().add(new JButton());
+                roomPlayers.get((kingIndex+1)%4).getMyButtons().getLast().setIcon(new ImageIcon(roomPlayers.get((kingIndex+1)%4).getMyCards().getLast().getRoo().getImage()));
+            }
+            waitForRulerCardSelection();
+            divideCards();
+            // start game method...
+
+
+    }
+    private void waitForRulerCardSelection() {
+        synchronized (lock) {
+            while (!isRulerCardSelected) {
+                try {
+                    lock.wait(); // منتظر می‌ماند تا حاکم کارت را انتخاب کند
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+
+    // put this method in each actionListeners of Cards button to check hokm is selected or not...
+    public void rulerCardSelected() {
+        synchronized (lock) {
+            isRulerCardSelected = true;
+            lock.notifyAll(); // اطلاع به نخ منتظر که کارت انتخاب شده است
+        }
+    }
 
     public void divideCards(){
-        while (!roomCards.isEmpty()){
-            for (int i=0 ; i < 4 ; i++){
-                for (int j = 0; j < 5; j++) {
-                  roomPlayers.get(i).getMyCards().add(roomCards.get(rand.nextInt(roomCards.size())));
+        // دادن 5 کارت به 2 نفر بعدی
+        for (int j = 0; j < 5; j++) {
+            roomPlayers.get((kingIndex+2)%4).getMyCards().add(roomCards.get(rand.nextInt(roomCards.size())));
+            roomPlayers.get((kingIndex+2)%4).getMyButtons().add(new JButton());
+            roomPlayers.get((kingIndex+2)%4).getMyButtons().getLast().setIcon(new ImageIcon(roomPlayers.get((kingIndex+2)%4).getMyCards().getLast().getRoo().getImage()));
+        }
+        for (int j = 0; j < 5; j++) {
+            roomPlayers.get((kingIndex+3)%4).getMyCards().add(roomCards.get(rand.nextInt(roomCards.size())));
+            roomPlayers.get((kingIndex+3)%4).getMyButtons().add(new JButton());
+            roomPlayers.get((kingIndex+3)%4).getMyButtons().getLast().setIcon(new ImageIcon(roomPlayers.get((kingIndex+3)%4).getMyCards().getLast().getRoo().getImage()));
+        }
+        // دادن 2 دور 4 کارت به هر 4 نفر
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 4; j++) {
+            int playerIndex = (kingIndex + j) % 4;
+                for (int k = 0; k < 4; k++) {
+                    roomPlayers.get(playerIndex).getMyCards().add(roomCards.get(rand.nextInt(roomCards.size())));
+                    roomPlayers.get(playerIndex).getMyButtons().add(new JButton());
+                    roomPlayers.get(playerIndex).getMyButtons().getLast().setIcon(new ImageIcon(roomPlayers.get(playerIndex).getMyCards().getLast().getRoo().getImage()));
                 }
-            }
-            //ایجاد وقفه برای تعیین حکم
-            for (int i=0 ; i < 4 ; i++){
-                for (int j = 0; j < 4; j++) {
-                    roomPlayers.get(i).getMyCards().add(roomCards.get(rand.nextInt(roomCards.size())));
-                }
-            }
-            for (int i=0 ; i < 4 ; i++){
-                for (int j = 0; j < 4; j++) {
-                    roomPlayers.get(i).getMyCards().add(roomCards.get(rand.nextInt(roomCards.size())));
-                }
-            }
+             }
+
         }
     }
 }
