aaf6245f4e2c6a941c42cca7be34862ef1411969
arshiammz <mohammadzade.arshia84@gmail.com>
2024-06-29 23:57:54 +0330

    coments deleted.

--- src/main/java/GamePlay/Game.java
+++ src/main/java/GamePlay/Game.java
@@ -10,9 +10,9 @@ import com.google.gson.Gson;
 
 import static Server.Server.sendMessageToOne;
 
-public class Game{
+public class Game {
     private String token;
-    private static  CardBox cardBox = new CardBox();
+    private static CardBox cardBox = new CardBox();
     private int round;
     private ClientHandler ruler;
     private Card hokm;
@@ -23,10 +23,12 @@ public class Game{
 
     public ArrayList<Team> roomTeams = new ArrayList<>(2);
     public static ArrayList<Card> roomCards = new ArrayList<>(getCardBox().cards);
-    public List<ClientHandler> roomPlayers ;
+    public List<ClientHandler> roomPlayers;
+
     public static CardBox getCardBox() {
         return cardBox;
     }
+
     public ArrayList<Card> getRoomCards() {
         return roomCards;
     }
@@ -34,8 +36,8 @@ public class Game{
     // Constructor
     public Game(List<ClientHandler> GameMembers) {
         this.roomPlayers = GameMembers;
-        roomTeams.add(new Team(roomPlayers.get(0),roomPlayers.get(2)));
-        roomTeams.add(new Team(roomPlayers.get(1),roomPlayers.get(3)));
+        roomTeams.add(new Team(roomPlayers.get(0), roomPlayers.get(2)));
+        roomTeams.add(new Team(roomPlayers.get(1), roomPlayers.get(3)));
         this.ruler = roomPlayers.get(rand.nextInt(roomPlayers.size()));
     }
 
@@ -57,19 +59,20 @@ public class Game{
     }
 
 
-    public void initializingNames(){
-        for (int i = 0; i <4; i++){
+    public void initializingNames() {
+        for (int i = 0; i < 4; i++) {
             roomPlayers.get(i).sendMessage("LEFT NAME:" + roomPlayers.get(i).getNickname());
-            roomPlayers.get(i).sendMessage("LEFT NAME:" + roomPlayers.get((i+1)%4).getNickname());
-            roomPlayers.get(i).sendMessage("FRONT NAME:" + roomPlayers.get((i+2)%4).getNickname());
-            roomPlayers.get(i).sendMessage("RIGHT NAME:" + roomPlayers.get((i+3)%4).getNickname());
+            roomPlayers.get(i).sendMessage("LEFT NAME:" + roomPlayers.get((i + 1) % 4).getNickname());
+            roomPlayers.get(i).sendMessage("FRONT NAME:" + roomPlayers.get((i + 2) % 4).getNickname());
+            roomPlayers.get(i).sendMessage("RIGHT NAME:" + roomPlayers.get((i + 3) % 4).getNickname());
         }
     }
+
     public void CardDividing() {
 
         int rulerIndex = 0;
         for (int i = 0; i < roomPlayers.size(); i++) {
-            if (ruler == roomPlayers.get(i)){
+            if (ruler == roomPlayers.get(i)) {
                 rulerIndex = i;
                 break;
             }
@@ -77,13 +80,13 @@ public class Game{
 
         int governingNumber = 0;
         for (ClientHandler player : roomPlayers) {
-            player.sendMessage( "Players " +roomPlayers.get(0).getNickname() + " 0 " + roomPlayers.get(1).getNickname() + " 1 " + roomPlayers.get(2).getNickname() + " 2 " + roomPlayers.get(3).getNickname() + " 3");
+            player.sendMessage("Players " + roomPlayers.get(0).getNickname() + " 0 " + roomPlayers.get(1).getNickname() + " 1 " + roomPlayers.get(2).getNickname() + " 2 " + roomPlayers.get(3).getNickname() + " 3");
             if (governingNumber == rulerIndex) {
                 player.sendMessage("You are ruler ");
             } else {
                 player.sendMessage("Ruler is " + roomPlayers.get(rulerIndex).getNickname()); // ارسال پیام حاکم به کل اعضای گروه
             }
-            governingNumber++ ;
+            governingNumber++;
 
         }
 
@@ -130,25 +133,6 @@ public class Game{
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     private void waitForRulerCardSelection() {
         synchronized (lock) {
             while (!isRulerCardSelected) {
@@ -168,352 +152,4 @@ public class Game{
             lock.notifyAll(); // اطلاع به نخ منتظر که کارت انتخاب شده است
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*
-        public void divideCards(){
-        int randomCard;
-        // دادن 5 کارت به 2 نفر بعدی
-        for (int j = 0; j < 5; j++) {
-            randomCard = rand.nextInt(roomCards.size());
-            roomPlayers.get((kingIndex+2)%4).getMyCards().add(roomCards.get(randomCard));
-            roomPlayers.get((kingIndex+2)%4).getMyButtons().add(new JButton());
-            //roomPlayers.get((kingIndex+2)%4).getMyButtons().getLast().setIcon(new ImageIcon(roomPlayers.get((kingIndex+2)%4).getMyCards().getLast().getRoo().getImage()));
-            roomPlayers.get((kingIndex+2)%4).showHandCards();
-            roomCards.remove(randomCard);
-        }
-        for (int j = 0; j < 5; j++) {
-            randomCard = rand.nextInt(roomCards.size());
-            roomPlayers.get((kingIndex+3)%4).getMyCards().add(roomCards.get(randomCard));
-            roomPlayers.get((kingIndex+3)%4).getMyButtons().add(new JButton());
-          //  roomPlayers.get((kingIndex+3)%4).getMyButtons().getLast().setIcon(new ImageIcon(roomPlayers.get((kingIndex+3)%4).getMyCards().getLast().getRoo().getImage()));
-            roomPlayers.get((kingIndex+3)%4).showHandCards();
-            roomCards.remove(randomCard);
-        }
-        // دادن 2 دور 4 کارت به هر 4 نفر
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 4; j++) {
-            int playerIndex = (kingIndex + j) % 4;
-                for (int k = 0; k < 4; k++) {
-                    randomCard = rand.nextInt(roomCards.size());
-                    roomPlayers.get(playerIndex).getMyCards().add(roomCards.get(rand.nextInt(roomCards.size())));
-                    roomPlayers.get(playerIndex).getMyButtons().add(new JButton());
-                    //roomPlayers.get(playerIndex).getMyButtons().getLast().setIcon(new ImageIcon(roomPlayers.get(playerIndex).getMyCards().getLast().getRoo().getImage()));
-                    roomPlayers.get(playerIndex).showHandCards();
-                    roomCards.remove(randomCard);
-                }
-             }
-
-        }
-    }
-     */
-
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-package GamePlay;
-import Server.Server.ClientHandler;
-import Server.Server;
-
-import javax.swing.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import com.google.gson.Gson;
-
-import static Server.Server.sendMessageToOne;
-
-public class Game{
-    private String token;
-    private static CardBox cardBox;
-    private int round;
-    private ClientHandler ruler;
-    private Card hokm;
-    Random rand = new Random();
-    Gson gson = new Gson();
-    private final Object lock = new Object();
-    private boolean isRulerCardSelected = false;
-
-    public ArrayList<Team> roomTeams = new ArrayList<>(2);
-    public ArrayList<Card> roomCards = new ArrayList<>(getCardBox().cards);
-    public List<ClientHandler> roomPlayers ;
-
-    // Constructor
-    public Game(List<ClientHandler> GameMembers) {
-        this.roomPlayers = GameMembers;
-        roomTeams.add(new Team(roomPlayers.get(0),roomPlayers.get(2)));
-        roomTeams.add(new Team(roomPlayers.get(1),roomPlayers.get(3)));
-        cardBox = new CardBox();
-        this.ruler = roomPlayers.get(rand.nextInt(roomPlayers.size()));
-    }
-
-    // Getter method for CardBox
-    public CardBox getCardBox() {
-        return cardBox;
-    }
-
-
-    public Card getHokm() {
-        return hokm;
-    }
-
-    public void setHokm(Card hokm) {
-        this.hokm = hokm;
-    }
-
-    public ClientHandler getKing() {
-        return ruler;
-    }
-
-    public void setKing(ClientHandler king) {
-        this.ruler = king;
-    }
-
-
-    public void initializingNames(){
-        for (int i = 0; i <4; i++){
-            sendMessageToOne("YOUR NAME:" + roomPlayers.get(i).nickname,roomPlayers.get(i));
-            roomPlayers.get(i).sendMessageToOne("LEFT NAME:" + roomPlayers.get((i+1)%4).nickname);
-            roomPlayers.get(i).sendMessageToOne("FRONT NAME:" + roomPlayers.get((i+2)%4).nickname);
-            roomPlayers.get(i).sendMessageToOne("RIGHT NAME:" + roomPlayers.get((i+3)%4).nickname);
-        }
-    }
-    public void CardDividing() {
-
-        int rulerIndex = 0;
-        for (int i = 0; i < roomPlayers.size(); i++) {
-            if (ruler == roomPlayers.get(i)){
-                rulerIndex = i;
-                break;
-            }
-        }
-
-        int governingNumber = 0;
-        for (ClientHandler player : roomPlayers) {
-            player.sendMessageToOne( "Players " +roomPlayers.get(0).nickname + " 0 " + roomPlayers.get(1).nickname + " 1 " + roomPlayers.get(2).nickname + " 2 " + roomPlayers.get(3).nickname + " 3");
-            if (governingNumber == rulerIndex) {
-                player.sendMessageToOne("You are ruler ");
-            } else {
-                player.sendMessageToOne("Ruler is " + roomPlayers.get(rulerIndex).nickname); // ارسال پیام حاکم به کل اعضای گروه
-            }
-            governingNumber++ ;
-
-        }
-
-        int randomCard;
-        // دادن 5 کارت به حاکم و نفر بعدیش
-        for (int j = 0; j < 5; j++) {
-            randomCard = rand.nextInt(roomCards.size());
-            String CodedRandomCard = gson.toJson(roomCards.get(randomCard));
-            roomPlayers.get(rulerIndex).sendMessageToOne("TAKE CARD:" + CodedRandomCard);
-            roomCards.remove(randomCard);
-        }
-        for (int j = 0; j < 5; j++) {
-            randomCard = rand.nextInt(roomCards.size());
-            String CodedRandomCard = gson.toJson(roomCards.get(randomCard));
-            roomPlayers.get((rulerIndex + 1) % 4).sendMessageToOne("TAKE CARD:" + CodedRandomCard);
-            roomCards.remove(randomCard);
-        }
-
-        // دادن 5 کارت به 2 نفر بعدی
-        for (int j = 0; j < 5; j++) {
-            randomCard = rand.nextInt(roomCards.size());
-            String CodedRandomCard = gson.toJson(roomCards.get(randomCard));
-            roomPlayers.get((rulerIndex + 2) % 4).sendMessageToOne("TAKE CARD:" + CodedRandomCard);
-            roomCards.remove(randomCard);
-        }
-        for (int j = 0; j < 5; j++) {
-            randomCard = rand.nextInt(roomCards.size());
-            String CodedRandomCard = gson.toJson(roomCards.get(randomCard));
-            roomPlayers.get((rulerIndex + 3) % 4).sendMessageToOne("TAKE CARD:" + CodedRandomCard);
-            roomCards.remove(randomCard);
-        }
-        // دادن 2 دور 4 کارت به هر 4 نفر
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 4; j++) {
-                int playerIndex = (rulerIndex + j) % 4;
-                for (int k = 0; k < 4; k++) {
-                    randomCard = rand.nextInt(roomCards.size());
-                    String CodedRandomCard = gson.toJson(roomCards.get(randomCard));
-                    roomPlayers.get(playerIndex).sendMessageToOne("TAKE CARD:" + CodedRandomCard);
-                    roomCards.remove(randomCard);
-                }
-            }
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    private void waitForRulerCardSelection() {
-        synchronized (lock) {
-            while (!isRulerCardSelected) {
-                try {
-                    lock.wait(); // منتظر می‌ماند تا حاکم کارت را انتخاب کند
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
-            }
-        }
-    }
-
-    // put this method in each actionListeners of Cards button to check hokm is selected or not...
-    public void rulerCardSelected() {
-        synchronized (lock) {
-            isRulerCardSelected = true;
-            lock.notifyAll(); // اطلاع به نخ منتظر که کارت انتخاب شده است
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*
-        public void divideCards(){
-        int randomCard;
-        // دادن 5 کارت به 2 نفر بعدی
-        for (int j = 0; j < 5; j++) {
-            randomCard = rand.nextInt(roomCards.size());
-            roomPlayers.get((kingIndex+2)%4).getMyCards().add(roomCards.get(randomCard));
-            roomPlayers.get((kingIndex+2)%4).getMyButtons().add(new JButton());
-            //roomPlayers.get((kingIndex+2)%4).getMyButtons().getLast().setIcon(new ImageIcon(roomPlayers.get((kingIndex+2)%4).getMyCards().getLast().getRoo().getImage()));
-            roomPlayers.get((kingIndex+2)%4).showHandCards();
-            roomCards.remove(randomCard);
-        }
-        for (int j = 0; j < 5; j++) {
-            randomCard = rand.nextInt(roomCards.size());
-            roomPlayers.get((kingIndex+3)%4).getMyCards().add(roomCards.get(randomCard));
-            roomPlayers.get((kingIndex+3)%4).getMyButtons().add(new JButton());
-          //  roomPlayers.get((kingIndex+3)%4).getMyButtons().getLast().setIcon(new ImageIcon(roomPlayers.get((kingIndex+3)%4).getMyCards().getLast().getRoo().getImage()));
-            roomPlayers.get((kingIndex+3)%4).showHandCards();
-            roomCards.remove(randomCard);
-        }
-        // دادن 2 دور 4 کارت به هر 4 نفر
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 4; j++) {
-            int playerIndex = (kingIndex + j) % 4;
-                for (int k = 0; k < 4; k++) {
-                    randomCard = rand.nextInt(roomCards.size());
-                    roomPlayers.get(playerIndex).getMyCards().add(roomCards.get(rand.nextInt(roomCards.size())));
-                    roomPlayers.get(playerIndex).getMyButtons().add(new JButton());
-                    //roomPlayers.get(playerIndex).getMyButtons().getLast().setIcon(new ImageIcon(roomPlayers.get(playerIndex).getMyCards().getLast().getRoo().getImage()));
-                    roomPlayers.get(playerIndex).showHandCards();
-                    roomCards.remove(randomCard);
-                }
-             }
-
-        }
-    }
-     */
-
-
-
-
