8afe0aade867dfe99ab116fab3331d3059941443
arshiammz <mohammadzade.arshia84@gmail.com>
2024-07-08 14:30:21 +0330

    game index added for mr.server

--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -52,6 +52,7 @@ public class Server {
         private PrintWriter out;
         private String nickname;
         private int playerIndex;
+        private int gameIndex;
         private int winedSets = 0;
         private int winedRounds = 0;
 
@@ -61,6 +62,13 @@ public class Server {
 
         public void setPlayerIndex(int playerIndex) {
             this.playerIndex = playerIndex;
+        }
+
+        public int getGameIndex() {
+            return gameIndex;
+        }
+        public void setGameIndex(int gameIndex){
+            this.gameIndex =gameIndex;
         }
 
         public ClientHandler(Socket socket) {
@@ -105,21 +113,21 @@ public class Server {
                     if (message.startsWith("I PUT:")){
                         String JsonCardString = message.substring(6);
                         Card card = gson.fromJson(JsonCardString, Card.class);
-                        AllGames.getLast().gameRounds.getLast().gameSets.getLast().getBordMap().put(playerIndex,card);
-                        if (AllGames.getLast().gameRounds.getLast().gameSets.getLast().getBordCards().isEmpty()){
-                            AllGames.getLast().gameRounds.getLast().gameSets.getLast().setBordType(card.getType());
+                        AllGames.get(gameIndex).gameRounds.getLast().gameSets.getLast().getBordMap().put(playerIndex,card);
+                        if (AllGames.get(gameIndex).gameRounds.getLast().gameSets.getLast().getBordCards().isEmpty()){
+                            AllGames.get(gameIndex).gameRounds.getLast().gameSets.getLast().setBordType(card.getType());
                         }
-                        AllGames.getLast().gameRounds.getLast().gameSets.getLast().bordCards.add(card);
-                        AllGames.getLast().gameRounds.getLast().gameSets.getLast().updateBordCards(JsonCardString,playerIndex);
-                        AllGames.getLast().gameRounds.getLast().gameSets.getLast().playerCardSelected();
+                        AllGames.get(gameIndex).gameRounds.getLast().gameSets.getLast().bordCards.add(card);
+                        AllGames.get(gameIndex).gameRounds.getLast().gameSets.getLast().updateBordCards(JsonCardString,playerIndex);
+                        AllGames.get(gameIndex).gameRounds.getLast().gameSets.getLast().playerCardSelected();
                     }
                     if (message.startsWith("RUL IS:")){
                         String rul = message.substring(7);
-                        AllGames.getLast().gameRounds.getLast().setRulType(rul);
+                        AllGames.get(gameIndex).gameRounds.getLast().setRulType(rul);
                         for (int i = 0; i < 4; i++) {
-                            AllGames.getLast().roomPlayers.get(i).sendMessage("RUL IS:" + rul);
+                            AllGames.get(gameIndex).roomPlayers.get(i).sendMessage("RUL IS:" + rul);
                         }
-                        AllGames.getLast().gameRounds.getLast().rulerCardSelected();
+                        AllGames.get(gameIndex).gameRounds.getLast().rulerCardSelected();
                     }
 
 
@@ -225,11 +233,16 @@ public class Server {
 
         // شروع بازی با گروه
         private void startGame(List<ClientHandler> group) {
+            Game newGame = new Game(group);
+            AllGames.add(newGame);
             group.get(0).setPlayerIndex(0);
             group.get(1).setPlayerIndex(1);
             group.get(2).setPlayerIndex(2);
             group.get(3).setPlayerIndex(3);
-            Game newGame = new Game(group);
+            group.get(0).setGameIndex(AllGames.size() - 1);
+            group.get(1).setGameIndex(AllGames.size() - 1);
+            group.get(2).setGameIndex(AllGames.size() - 1);
+            group.get(3).setGameIndex(AllGames.size() - 1);
             System.out.println("*****  " + group.size());
             AllGames.add(newGame);
             new Thread(() ->{
