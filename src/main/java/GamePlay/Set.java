93e06a45e3c2a6cb4963013544a2815a9c210452
arshiammz <mohammadzade.arshia84@gmail.com>
2024-07-24 12:40:07 +0330

    a bit better...

--- src/main/java/GamePlay/Set.java
+++ src/main/java/GamePlay/Set.java
@@ -83,12 +83,12 @@ public class Set {
     //Starting set method
     public void startSet() {
         puttingCard();
+        scoring();
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e){
             e.printStackTrace();
         }
-        scoring();
         cleaningBord();
         setNextFirstPlayer(round.getGame().roomPlayers.get(winner()));
         setIsSetFinished();
