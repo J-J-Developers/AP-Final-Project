0b0c98700c5374d2903665799f20d9e064540aec
arshiammz <mohammadzade.arshia84@gmail.com>
2024-07-24 22:31:49 +0330

    4 clients added to test runing more than 1 game in server.

---
+++ src/main/java/Client/Client5.java
@@ -0,0 +1,8 @@
+package Client;
+
+public class Client5 {
+    public static void main(String[] args) throws Exception {
+        Client client = new Client();
+        client.startClient();
+    }
+}
