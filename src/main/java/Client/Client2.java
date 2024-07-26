0105148d07c11518714189b2e3e41b1e79d07e32
Zeinabc7 <darya.abbasi1383@gmail.com>
2024-07-26 18:43:11 +0330

    .

--- src/main/java/Client/Client2.java
+++ src/main/java/Client/Client2.java
@@ -2,6 +2,7 @@ package Client;
 
 public class Client2 {
     public static void main(String[] args) throws Exception {
+
         Client client = new Client();
         client.startClient();
     }
